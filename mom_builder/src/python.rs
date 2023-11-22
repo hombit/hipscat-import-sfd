0524fe395b8e09e8dd32fb86c80355c57b7fbf15
Konstantin Malanchev <hombit@gmail.com>
2023-11-22 16:09:51 -0500

    New interface for mom builder

--- mom_builder/src/python.rs
+++ mom_builder/src/python.rs
@@ -2,8 +2,11 @@ use crate::build_tree::build_tree;
 use crate::state::{
     MinMaxMeanState, MinMaxMeanStateMerger, MinMaxMeanStateValidator, StateBuilder,
 };
+use std::collections::BTreeMap;
 use std::convert::Infallible;
+use std::sync::RwLock;
 
+use crate::tree::Tree;
 use crate::tree_config::TreeConfig;
 use itertools::Itertools;
 use numpy::ndarray::{Array1 as NdArray, ArrayView1 as NdArrayView, NdFloat};
@@ -27,7 +30,7 @@ fn py_mom_from_array<'py>(
 fn mom_from_array<T>(
     values: NdArrayView<T>,
     max_norder: usize,
-    thereshold: T,
+    threshold: T,
 ) -> Vec<(NdArray<usize>, NdArray<T>)>
 where
     T: NdFloat,
@@ -35,7 +38,7 @@ where
 {
     let it = values.iter().map(|&x| -> Result<T, Infallible> { Ok(x) });
 
-    mom_from_it(it, max_norder, thereshold).expect("Should not fail with infallible error")
+    mom_from_it(it, max_norder, threshold).expect("Should not fail with infallible error")
 }
 
 #[pyfunction(name = "mom_from_batch_it")]
@@ -63,7 +66,7 @@ fn py_mom_from_batch_it<'py>(
 fn mom_from_it<'a, T, E>(
     it: impl Iterator<Item = Result<T, E>>,
     max_norder: usize,
-    thereshold: T,
+    threshold: T,
 ) -> Result<Vec<(NdArray<usize>, NdArray<T>)>, E>
 where
     T: NdFloat,
@@ -75,7 +78,7 @@ where
 
     let state_builder = StateBuilder::new(
         MinMaxMeanStateMerger::new(),
-        MinMaxMeanStateValidator::new(thereshold),
+        MinMaxMeanStateValidator::new(threshold),
     );
 
     let tree_config = TreeConfig::new(12usize, 4usize, max_norder);
@@ -95,10 +98,193 @@ where
     Ok(output)
 }
 
+#[pyclass]
+struct MomBuilder {
+    #[pyo3(get)]
+    intermediate_norder: usize,
+    #[pyo3(get)]
+    max_norder: usize,
+    intermediate_states: RwLock<BTreeMap<usize, Option<MinMaxMeanState<f64>>>>,
+    intermediate_tree_config: TreeConfig,
+    top_tree_config: TreeConfig,
+    state_builder: StateBuilder<MinMaxMeanStateMerger<f64>, MinMaxMeanStateValidator<f64>>,
+}
+
+impl MomBuilder {
+    fn tree_to_python<'py>(
+        py: Python<'py>,
+        tree: Tree<MinMaxMeanState<f64>>,
+    ) -> Vec<(&'py PyArray1<usize>, &'py PyArray1<f64>)> {
+        tree.into_iter()
+            .map(|tiles| {
+                let (indexes, values) = tiles.into_tuple();
+                (
+                    NdArray::from_vec(indexes).into_pyarray(py),
+                    values
+                        .into_iter()
+                        .map(|x| x.into())
+                        .collect::<NdArray<_>>()
+                        .into_pyarray(py),
+                )
+            })
+            .collect()
+    }
+
+    fn max_norder_index_offset(&self, intermediate_index: usize) -> usize {
+        intermediate_index * self.intermediate_tree_config.max_norder_ntiles()
+    }
+}
+
+#[pymethods]
+impl MomBuilder {
+    #[new]
+    fn __new__(intermediate_norder: usize, max_norder: usize, threshold: f64) -> PyResult<Self> {
+        if intermediate_norder > max_norder {
+            return Err(pyo3::exceptions::PyValueError::new_err(
+                "intermediate_norder must be less than or equal to max_norder",
+            ));
+        }
+
+        let state_builder = StateBuilder::new(
+            MinMaxMeanStateMerger::new(),
+            MinMaxMeanStateValidator::new(threshold),
+        );
+
+        Ok(Self {
+            intermediate_states: RwLock::new(BTreeMap::new()),
+            intermediate_norder,
+            max_norder,
+            intermediate_tree_config: TreeConfig::new(
+                1usize,
+                4usize,
+                max_norder - intermediate_norder,
+            ),
+            top_tree_config: TreeConfig::new(12usize, 4usize, intermediate_norder),
+            state_builder,
+        })
+    }
+
+    #[getter]
+    fn intermediate_ntiles(&self) -> usize {
+        self.top_tree_config.max_norder_ntiles()
+    }
+
+    fn subtree_maxnorder_indexes<'py>(
+        &self,
+        py: Python<'py>,
+        intermediate_index: usize,
+    ) -> PyResult<&'py PyArray1<usize>> {
+        if intermediate_index >= self.top_tree_config.max_norder_ntiles() {
+            return Err(pyo3::exceptions::PyValueError::new_err(
+                "intermediate_index is out of range",
+            ));
+        }
+        let offset = self.max_norder_index_offset(intermediate_index);
+
+        let output = PyArray1::from_vec(
+            py,
+            (offset..offset + self.intermediate_tree_config.max_norder_ntiles())
+                .map(|i| i)
+                .collect(),
+        );
+        Ok(output)
+    }
+
+    fn build_subtree<'py>(
+        &self,
+        py: Python<'py>,
+        intermediate_index: usize,
+        a: PyReadonlyArray1<f64>,
+    ) -> PyResult<Vec<(&'py PyArray1<usize>, &'py PyArray1<f64>)>> {
+        if self
+            .intermediate_states
+            .read()
+            .expect("Cannot lock states storage for read")
+            .contains_key(&intermediate_index)
+        {
+            return Err(pyo3::exceptions::PyValueError::new_err(
+                "State with this index already exists",
+            ));
+        }
+
+        // We need to build a subtree with a single root node, we will accumulate all the nodes later
+        // Current subtree has an offset in indexes on the deepest level (maximum norder)
+        let index_offset = self.max_norder_index_offset(intermediate_index);
+
+        let array = a.as_array();
+
+        let it_states =
+            array
+                .iter()
+                .enumerate()
+                .map(|(relative_index, &x)| -> Result<_, Infallible> {
+                    Ok((index_offset + relative_index, MinMaxMeanState::new(x)))
+                });
+        let mut tree = build_tree(
+            self.state_builder,
+            self.intermediate_tree_config.clone(),
+            it_states,
+        )?;
+
+        // Extract root node from the tree, it should have at most one state
+        let root_tiles = tree.remove(0);
+        let state = match root_tiles.len() {
+            0 => None,
+            1 => {
+                let (indexes, states) = root_tiles.into_tuple();
+                assert_eq!(indexes[0], intermediate_index);
+                Some(states[0])
+            }
+            _ => panic!("Root node should have at most one state"),
+        };
+        self.intermediate_states
+            .write()
+            .expect("Cannot lock states storage for write")
+            .insert(intermediate_index, state);
+
+        // Return the rest of the subtree
+        Ok(Self::tree_to_python(py, tree))
+    }
+
+    fn build_top_tree<'py>(
+        &self,
+        py: Python<'py>,
+    ) -> PyResult<Vec<(&'py PyArray1<usize>, &'py PyArray1<f64>)>> {
+        if self
+            .intermediate_states
+            .read()
+            .expect("Cannot lock states storage for read")
+            .keys()
+            .enumerate()
+            .any(|(i, index)| *index != i)
+        {
+            return Err(pyo3::exceptions::PyValueError::new_err(
+                "Intermediate tiles are not contiguous",
+            ));
+        }
+
+        let states = std::mem::take(
+            &mut *self
+                .intermediate_states
+                .write()
+                .expect("Cannot lock states storage for write"),
+        );
+
+        let it_states = states.into_iter().filter_map(|(index, state)| {
+            state.map(|state| -> Result<_, Infallible> { Ok((index, state)) })
+        });
+
+        let tree = build_tree(self.state_builder, self.top_tree_config.clone(), it_states)?;
+
+        Ok(Self::tree_to_python(py, tree))
+    }
+}
+
 #[pymodule]
 fn mom_builder(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(py_mom_from_array, m)?)?;
     m.add_function(wrap_pyfunction!(py_mom_from_batch_it, m)?)?;
+    m.add_class::<MomBuilder>()?;
     Ok(())
 }
 
--- mom_builder/src/tree_config.rs
+++ mom_builder/src/tree_config.rs
@@ -4,6 +4,7 @@ pub(crate) struct TreeConfig {
     n_root: usize,
     n_children: usize,
     max_norder: usize,
+    max_norder_ntiles: usize,
 }
 
 impl TreeConfig {
@@ -15,11 +16,13 @@ impl TreeConfig {
         let n_root = n_root.into();
         let n_children = n_children.into();
         let max_norder = max_norder.into();
+        let max_norder_ntiles = n_root * n_children.pow(max_norder as u32);
 
         Self {
             n_root,
             n_children,
             max_norder,
+            max_norder_ntiles,
         }
     }
 
@@ -29,5 +32,9 @@ impl TreeConfig {
 
     pub(crate) fn max_norder(&self) -> usize {
         self.max_norder
+    }
+
+    pub(crate) fn max_norder_ntiles(&self) -> usize {
+        self.max_norder_ntiles
     }
 }
